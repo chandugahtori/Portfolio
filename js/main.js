37681fafb018af4c0e0b5d61400691eda5b5a6bd
CHANDRA SHEKHAR <chandu2004gahtori@gmail.com>
2026-01-05 13:17:41 +0530

    initial

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const filterButtons = document.querySelectorAll(".filter-btn");
+const projectCards = document.querySelectorAll(".project-card");
+
+filterButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+
+    filterButtons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const filter = btn.getAttribute("data-filter");
+
+    projectCards.forEach(card => {
+      if (filter === "all" || card.classList.contains(filter)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
